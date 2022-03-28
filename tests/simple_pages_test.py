e5d1338a1d8f2171f9fee402ba194bd7b7db2840
keith <keithcicala123@gmail.com>
2022-03-28 11:47:27 -0400

    i think i fixed it this time simple pages test

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -5,7 +5,6 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/index">Home</a>' in response.data
     assert b'<a class="nav-link" href="/aaaTesting">AAA Testing</a>' in response.data
     assert b'<a class="nav-link" href="/cicd">CI/CD</a>' in response.data
     assert b'<a class="nav-link" href="/docker">Docker</a>' in response.data
@@ -18,7 +17,7 @@ def test_request_main_menu_links(client):
 
 def test_request_index(client):
     """This makes the index page"""
-    response = client.get("/index")
+    response = client.get("/")
     assert response.status_code == 200
     assert b"Home" in response.data
 
