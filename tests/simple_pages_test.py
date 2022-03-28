1ea5564d702934914b2c5ef2c830f9c1cf173eea
keith <keithcicala123@gmail.com>
2022-03-27 23:40:57 -0400

    actually fixed simple pages test

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -5,14 +5,14 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/index">Index</a>' in response.data
-    assert b'<a class="nav-link" href="/aaaTesting">AAA</a>' in response.data
-    assert b'<a class="nav-link" href="/cicd">CICD</a>' in response.data
-    assert b'<a class="nav-link" href="/docker">DOCKER</a>' in response.data
-    assert b'<a class="nav-link" href="/git">GIT</a>' in response.data
-    assert b'<a class="nav-link" href="/oops">OOP</a>' in response.data
-    assert b'<a class="nav-link" href="/pylintAndOthers">Pylint</a>' in response.data
-    assert b'<a class="nav-link" href="/pythonFlask">PythonFlask</a>' in response.data
+    assert b'<a class="nav-link" href="/index">Home</a>' in response.data
+    assert b'<a class="nav-link" href="/aaaTesting">AAA Testing</a>' in response.data
+    assert b'<a class="nav-link" href="/cicd">CI/CD</a>' in response.data
+    assert b'<a class="nav-link" href="/docker">Docker</a>' in response.data
+    assert b'<a class="nav-link" href="/git">Git</a>' in response.data
+    assert b'<a class="nav-link" href="/oops">OOPS</a>' in response.data
+    assert b'<a class="nav-link" href="/pylintAndOthers">Pylint and Others</a>' in response.data
+    assert b'<a class="nav-link" href="/pythonFlask">Python/Flask</a>' in response.data
     assert b'<a class="nav-link" href="/solid">SOLID</a>' in response.data
 
 
@@ -20,56 +20,56 @@ def test_request_index(client):
     """This makes the index page"""
     response = client.get("/index")
     assert response.status_code == 200
-    assert b"Index" in response.data
+    assert b"Home" in response.data
 
 
 def test_request_aaa(client):
     """This makes the index page"""
     response = client.get("/aaaTesting")
     assert response.status_code == 200
-    assert b"AAA" in response.data
+    assert b"AAA Testing" in response.data
 
 
 def test_request_cicd(client):
     """This makes the index page"""
     response = client.get("/cicd")
     assert response.status_code == 200
-    assert b"CICD" in response.data
+    assert b"CI/CD" in response.data
 
 
 def test_request_docker(client):
     """This makes the index page"""
     response = client.get("/docker")
     assert response.status_code == 200
-    assert b"DOCKER" in response.data
+    assert b"Docker" in response.data
 
 
 def test_request_git(client):
     """This makes the index page"""
     response = client.get("/git")
     assert response.status_code == 200
-    assert b"GIT" in response.data
+    assert b"Git" in response.data
 
 
 def test_request_oop(client):
     """This makes the index page"""
     response = client.get("/oops")
     assert response.status_code == 200
-    assert b"OOP" in response.data
+    assert b"OOPS" in response.data
 
 
 def test_request_pylint(client):
     """This makes the index page"""
     response = client.get("/pylintAndOthers")
     assert response.status_code == 200
-    assert b"Pylint" in response.data
+    assert b"Pylint and Others" in response.data
 
 
 def test_request_pythonFlask(client):
     """This makes the index page"""
     response = client.get("/pythonFlask")
     assert response.status_code == 200
-    assert b"PythonFlask" in response.data
+    assert b"Python/Flask" in response.data
 
 
 def test_request_solid(client):
